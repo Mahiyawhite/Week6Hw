0d142e3f49702a3fcf3d914e64017e7ff752ff79
Mahiwhite <vdprajapti23@gmail.com>
2024-04-08 22:46:45 +0100

    my first push to repo

---
+++ JavaWeek6Hw/src/Ans20.java
@@ -0,0 +1,16 @@
+public class Ans20 {
+    //Write a program to add 5 and 6.2, and print in console
+
+        public static void main(String[] args) {
+            // Define the numbers
+            double number1 = 5;
+            double number2 = 6.2;
+
+            // Perform the addition
+            double sum = number1 + number2;
+
+            // Print the result to the console
+            System.out.println("Sum is: " + sum);
+
+    }
+}
