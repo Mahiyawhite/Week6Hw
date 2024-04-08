c56e654c0f912f154221c6a641796ec5d414283e
Mahiwhite <vdprajapti23@gmail.com>
2024-04-08 22:23:16 +0100

    my first push to repo

---
+++ JavaWeek6Hw/src/Ans11.java
@@ -0,0 +1,13 @@
+public class Ans11 {
+    public static void main(String[] args) {
+        //int x = 8;
+            for (int i = 1; i < 11; i++) {
+            int result = 8 * i;
+            System.out.println("8*"+i+"="+result);
+        }
+
+    }
+
+
+}
+
