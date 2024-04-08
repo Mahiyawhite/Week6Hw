e235fe9366f94313a229721b4b96a5cac03a4e38
Mahiwhite <vdprajapti23@gmail.com>
2024-04-08 22:34:27 +0100

    my first push to repo

---
+++ JavaWeek6Hw/src/Ans16.java
@@ -0,0 +1,12 @@
+public class Ans16 {
+    //Write program to display
+    //Test Data: ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
+    //Expected Output 2.138888888888889
+    public static void main(String[] args) {
+        // the calculation
+        double result = ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+
+        // Print the result
+        System.out.println("Result: " + result);
+    }
+}
