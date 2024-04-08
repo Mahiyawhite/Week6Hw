22ca123730adb830add151e626f3cb8c931373dd
Mahiwhite <vdprajapti23@gmail.com>
2024-04-08 22:27:56 +0100

    my first push to repo

--- JavaWeek6Hw/src/Ans11.java
+++ JavaWeek6Hw/src/Ans11.java
@@ -1,4 +1,13 @@
 public class Ans11 {
+    //Write a Java program that takes a number as input and prints its multiplication
+    //table up to 10.
+    //Expected Output :
+    //8 x 1 = 8
+    //8 x 2 = 16
+    //8 x 3 = 24
+    //...
+    //8 x 10 = 80
+
     public static void main(String[] args) {
         //int x = 8;
             for (int i = 1; i < 11; i++) {
