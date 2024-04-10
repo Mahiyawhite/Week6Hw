9fa69f5b7649c1b9b2ec7d83faf7dd4b3c0e53d1
Mahiwhite <vdprajapti23@gmail.com>
2024-04-10 23:59:03 +0100

    my first push to repo

---
+++ JavaWeek6Hw/src/HwAns9.java
@@ -0,0 +1,11 @@
+public class HwAns9 {
+    //9. Write a program to convert the upper case to lower case.
+
+        public static void main(String[] args) {
+            String word = "RAINBOW";
+            System.out.println(word.toUpperCase());
+            System.out.println(word.toLowerCase());
+        }
+}
+
+
