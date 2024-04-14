6bc3438fbdb2ec8676b9bded6b452ef18215af56
Mahiwhite <vdprajapti23@gmail.com>
2024-04-14 23:09:09 +0100

    my first push to repo

---
+++ JavaWeek6Hw/src/week7hw/Ans2.java
@@ -0,0 +1,14 @@
+package week7hw;
+/*Declare array and store any 5 countries and print them in console.*/
+public class Ans2 {
+    public static void main(String[] args) {
+
+        String[] countrys = {"India", "France", "United Kingdom", "USA", "Spain"};
+        System.out.println(countrys[0]);
+        System.out.println(countrys[1]);
+        System.out.println(countrys[2]);
+        System.out.println(countrys[3]);
+        System.out.println(countrys[4]);
+
+    }
+}
