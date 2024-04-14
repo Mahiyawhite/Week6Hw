7c6ab413b6a053aa74261dafd37576239f67ec37
Mahiwhite <vdprajapti23@gmail.com>
2024-04-15 00:20:11 +0100

    my first push to repo

---
+++ JavaWeek6Hw/src/week7hw/Ans3.java
@@ -0,0 +1,16 @@
+package week7hw;
+  /*Declare multidimensional array and store 5 countries and their capital and print them in
+    console.*/
+public class Ans3 {
+      public static void main(String[] args) {
+          String[][] mycountry = {{"India,canada,Uk,Usa,France"},
+                                  {"Delhi,Otava,London,Newyork,Paris"}};
+
+
+          System.out.println("mycountry and capital:");
+          for (int i = 0; i < mycountry.length; ++i) {
+              for (int j = 0; j < mycountry[i].length; ++j)
+                  System.out.println(mycountry[i][j]);
+          }
+      }
+}
