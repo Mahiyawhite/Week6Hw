890d497ca0bc7a48f9d5e73019ced3ca43665ae3
Mahiwhite <vdprajapti23@gmail.com>
2024-04-15 00:46:42 +0100

    my first push to repo

---
+++ JavaWeek6Hw/src/week7hw/A3.java
@@ -0,0 +1,23 @@
+package week7hw;
+     /*Declare multidimensional array and store 5 countries and their capital and print them in
+       console.*/
+public class A3 {
+       public static void main(String[] args) {
+
+            String[][] countriesAndCapitals = {
+                    {"India", "Delhi"},
+                    {"Canada", "Ottawa"},
+                    {"United Kingdom", "London"},
+                    {"France", "Paris"},
+                    {"United States", "Washington"}
+            };
+
+
+            System.out.println("Countries and Capitals:");
+            for (int i = 0; i < countriesAndCapitals.length; i++) {
+                System.out.println(countriesAndCapitals[i][0] + " - " + countriesAndCapitals[i][1]);
+            }
+        }
+    }
+
+
