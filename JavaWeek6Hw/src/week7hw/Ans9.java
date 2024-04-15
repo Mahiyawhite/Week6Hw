b78234c30fe4a43837ddfca2dc3a0d9559c38780
Mahiwhite <vdprajapti23@gmail.com>
2024-04-15 22:25:02 +0100

    my first push to repo

---
+++ JavaWeek6Hw/src/week7hw/Ans9.java
@@ -0,0 +1,13 @@
+package week7hw;
+/*Write java program using for loop and print your group name 10 times.*/
+public class Ans9 {
+    public static void main(String[] args) {
+        String Groupname = " Chamber of Secrets";
+
+        for (int i = 1; i < 11; i++) {
+            System.out.print(i);
+            System.out.println(Groupname);
+        }
+
+    }
+}
