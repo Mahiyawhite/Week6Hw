78153ec85231f6e69480b9cf0491a8c50e65b370
Mahiwhite <vdprajapti23@gmail.com>
2024-04-15 23:37:16 +0100

    my first push to repo

---
+++ JavaWeek6Hw/src/week7hw/Ans11.java
@@ -0,0 +1,34 @@
+package week7hw;
+/* Input any alphabet from “A" to “F” and print their city name accordingly (use if else) ifany
+other alphabet should be invalid entry. */
+
+import java.util.Scanner;
+
+public class Ans11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter alphabet between A to F :");
+        char alphabet = scanner.next().charAt(0);
+
+        String cityName;
+        if (alphabet == 'A') {
+            cityName = "Ahmedabad";
+        } else if (alphabet == 'B') {
+            cityName = "Bombay";
+        } else if (alphabet == 'C') {
+            cityName = "Chennai";
+        } else if (alphabet == 'D') {
+            cityName = "Dubai";
+        } else if (alphabet == 'E') {
+            cityName = "England";
+        } else if (alphabet == 'F') {
+            cityName = "Florida";
+        } else {
+            cityName = "Invalid entry";
+        }
+
+        System.out.println("City Name: " + cityName);
+
+        scanner.close();
+    }
+}
