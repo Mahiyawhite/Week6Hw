057dbb4406817d9c7d6335b08d45833070bc3eaa
Mahiwhite <vdprajapti23@gmail.com>
2024-04-15 21:34:34 +0100

    my first push to repo

---
+++ JavaWeek6Hw/src/week7hw/Ans4.java
@@ -0,0 +1,18 @@
+package week7hw;
+    /*Write if else condition and print your group name in console else others group name.*/
+public class Ans4 {
+
+        public static void main(String[] args) {
+
+           String Mygroupname = "Spice Girls";
+            String Othergroupname = "Sweet Girls";
+
+            if (Mygroupname == (Othergroupname)){
+                System.out.println("Mygroupname " + "Spice Girls");
+            } else {
+            System.out.println("Othergroup " + "Sweet Girls");}
+        }
+
+
+}
+
