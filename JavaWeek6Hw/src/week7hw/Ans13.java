8e64744c6b41599516a778c5b8b1fd42173a9179
Mahiwhite <vdprajapti23@gmail.com>
2024-04-16 01:36:24 +0100

    my first push to repo

---
+++ JavaWeek6Hw/src/week7hw/Ans13.java
@@ -0,0 +1,46 @@
+package week7hw;
+/* Write a java program to input any two number and ask user to enter their symbol (+, -, /, *)
+find addition, Subtraction, multiplication and division according to theirsymbol(using if else)*/
+
+import java.util.Scanner;
+
+public class Ans13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double number1 = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double number2 = sc.nextDouble();
+
+        System.out.print("Enter the symbol (+, -, *, /): ");
+        char action = sc.next().charAt(0);
+
+        double result;
+        if (action == '+') {
+            result = number1 + number2;
+        } else if (action == '-') {
+            result = number1 - number2;
+        } else if (action == '*') {
+            result = number1 * number2;
+        } else if (action == '/') {
+
+            if (number2 != 0) {
+                result = number1 / number2;
+            } else {
+                System.out.println("Error: Division by zero!");
+                return;
+            }
+        }
+              else {
+            System.out.println("Error: Invalid symbol!");
+            return;
+        }
+
+        System.out.println("Result: " + result);
+
+        sc.close();
+
+    }
+}
