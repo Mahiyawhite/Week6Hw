59f5e9c91320b8e4a6f3d8475fe805c3940ec4f8
Mahiwhite <vdprajapti23@gmail.com>
2024-04-19 01:44:49 +0100

    my first push to repo

---
+++ JavaWeek6Hw/src/midweek7hw/Que11.java
@@ -0,0 +1,14 @@
+package midweek7hw; /* Write a program to print a string entered by user. */
+
+import java.util.Scanner;
+
+public class Que11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any word: ");
+        String word = sc.next();
+        System.out.println("My word is: " + word);
+
+
+    }
+}
---
+++ JavaWeek6Hw/src/midweek7hw/Que12.java
@@ -0,0 +1,8 @@
+package midweek7hw; /* Write a program to find the length of the string "refrigerator". */
+
+public class Que12 {
+    public static void main(String[] args) {
+        String txt = "refrigerator";
+        System.out.println("The length of the txt string is: " + txt.length());
+    }
+}
